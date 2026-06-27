7ff3b21e4883ff73fd26867ca18942c31d5ac280
Joao Henrique Fontes <jhfontes@uchicago.edu>
2026-06-27 20:47:37 +0200

    lifetime measurement

--- qsample/sampler/splitting.py
+++ qsample/sampler/splitting.py
@@ -189,6 +189,8 @@ class SplittingSampler:
         self.partitions = {cid: self.err_model.group(circuit) for cid, circuit in self.protocol.circuits.items()}
         constants = {cid: math.subset_probs(circuit, self.err_model, self.p_max) for cid, circuit in protocol.circuits.items()}
         self.tree = Tree(constants)
+
+        self.failing_circuits = []
       
     def err_params_to_matrix(self, err_params):
         sorted_params = [err_params[k] for k in self.err_model.groups]
@@ -349,6 +351,7 @@ class SplittingSampler:
 
         if np.random.rand(1)<q:
             if self.run(new_circuit):
+                self.failing_circuits.append(new_circuit)
                 self.current_errors.append(new_circuit)
                 self.pi_E = pi_E_new
                 self.subsets.append(new_subset)
@@ -358,7 +361,6 @@ class SplittingSampler:
         pnode = self.protocol.root # get protocol start node
         state = self.simulator(max(self.protocol.qubits)+1) # init state
         msmt_hist = {} # init measurement history
-        
         while True:
             pnode, circuit = self.protocol.successor(pnode, msmt_hist)
             if circuit != None:
@@ -367,8 +369,187 @@ class SplittingSampler:
                 else:
                     msmt = state.run(circuit, fault_circuit)
 
-                
                 msmt = msmt if msmt==None else int(msmt,2) # convert to int for comparison in checks
                 msmt_hist[pnode] = msmt_hist.get(pnode, []) + [msmt]
             else:
                 return (pnode != None)
+            
+
+
+class LifetimeSampler:
+    
+    def __init__(self, n_shots, protocol, simulator, p_max, err_model, distance=None):
+
+        self.protocol = protocol
+        self.simulator = simulator
+        
+        # Calculate initial failure probability P(p1) using Direct Monte Carlo sampling
+        self.monte_carlo=DirectSampler(self.protocol, self.simulator, err_model, p_max)
+        self.E_0, self.circuit = self.monte_carlo.run(n_shots) # Modified .run from original function
+        self.logical_p = [self.monte_carlo.stats()[0]] # P(p1)
+        self.logical_error = [self.monte_carlo.stats()[1]] # epsilon
+
+        self.err_model = err_model()
+        self.p_max = self.err_params_to_matrix(p_max) # p1
+        self.distance = distance
+        
+        self.partitions = {cid: self.err_model.group(circuit) for cid, circuit in self.protocol.circuits.items()}
+        constants = {cid: math.subset_probs(circuit, self.err_model, self.p_max) for cid, circuit in protocol.circuits.items()}
+        self.tree = Tree(constants)
+      
+    def err_params_to_matrix(self, err_params):
+        sorted_params = [err_params[k] for k in self.err_model.groups]
+        return np.array(np.broadcast_arrays(*sorted_params)).T
+        
+    def stats(self, err_params=None):
+
+        _constants = self.tree.constants
+        prob = self.err_params if err_params == None else self.err_params_to_matrix(err_params)
+        self.tree.constants = {cid: math.subset_probs(circuit, self.err_model, prob) for cid, circuit in self.protocol.circuits.items()}
+        
+        p_L = self.tree.subtree_sum(self.tree.root, self.tree.marked)
+        delta = self.tree.delta
+        var = self.tree.var(mode=1)
+        var_up = self.tree.var(mode=0)
+        
+        self.tree.constants = _constants
+        return np.broadcast_arrays(p_L, np.sqrt(var), p_L+delta, np.sqrt(var_up))
+        
+    def save(self, path):
+        utils.save(self, path)
+        
+    def calculate_subset(self, circuits):
+        subset = 0
+        for circuit in circuits:
+            for i in list(circuit._ticks):
+                if i: subset+=1
+        return subset
+
+    def calculate_lifetime(self, n_rounds, t_init, p, seed=None):
+        """
+        
+        **Attributes:**
+    
+        """
+        if seed is None:
+            seed = int((time() * 1000000000) % (2**32 - 1))
+        np.random.seed(seed)
+        
+        self.physical_p = p 
+        E_0_subset=self.calculate_subset([self.E_0])
+        self.t_init = t_init # Initial Markov chain length
+
+        
+        N = len(self.circuit)
+
+        # pi(E) for n_rounds of circuits
+        self.subset_probs = p**np.arange(N*n_rounds)*(1-p)**(np.arange(N*n_rounds)[::-1])
+
+        # Create first Markov chain
+        self.subsets = [[E_0_subset]*n_rounds] 
+        self.pi_E=self.subset_probs[E_0_subset*n_rounds] # pi_1(E0)
+        self.failing_circuits = [[self.E_0]*n_rounds]
+
+        # Begin with a failure configuration that fails on the first round
+        self.failing_times = [1]
+        
+        t = t_init
+    
+        while len(self.failing_circuits)<t:
+            self.get_next_element(n_rounds)
+
+        self.failing_times = np.array(self.failing_times)
+
+        return np.average(self.failing_times)
+        
+        """
+        ## HOW DO I MAKE AN EQUIVALENT CALCULATION FOR THE LIFETIME MEASUREMENT
+        if j<n_steps-1:
+            samples_minus = self.g(self.subset_probs[j, self.subsets[1:]]/
+                                    self.subset_probs[j-1, self.subsets[1:]])
+            samples_plus = self.g(self.subset_probs[j, self.subsets[1:]]/
+                                    self.subset_probs[j+1, self.subsets[1:]])
+            
+            g_minus = np.sum(samples_minus)/t
+            g_plus = np.sum(samples_plus)/t
+
+            s_minus = np.sqrt(np.sum((samples_minus-g_minus)**2)/(t-1))
+            s_plus = np.sqrt(np.sum((samples_plus-g_plus)**2)/(t-1))
+
+            sigma = max(s_plus/g_plus, s_minus/g_minus)/np.sqrt(t)
+
+            g_minus_ = np.sum(samples_minus[:int(t/2)])/int(t/2)
+            g_plus_ = np.sum(samples_plus[:int(t/2)])/int(t/2)
+
+            delta = max(abs(g_plus-g_plus_)/g_plus, abs(g_minus-g_minus_)/g_minus)
+            if sigma+delta > 0.25/np.sqrt(n_steps):
+                t += scaling*t_init
+                scaling*=2
+        """
+
+
+
+
+    def get_next_element(self, n_rounds): # Metropolis sampling step
+        
+        new_circuit_all = []
+        fault_round = np.random.randint(n_rounds)
+
+        for j in range(n_rounds):
+            if j==fault_round:
+                new_loc = self.err_model.get_next_circuit(self.partitions[self.circuit.id])
+                loc_circuit = self.err_model.run(self.circuit, new_loc)
+                new_circuit = deepcopy(self.failing_circuits[-1][j]) ## OLHAR AQUI
+                for i in range(len(loc_circuit._ticks)):
+                    if loc_circuit._ticks[i]:
+                        qubit_loc = list(loc_circuit[i].values())
+                        for ii in qubit_loc:
+                            if ii in list(new_circuit[i].values()):
+                                index = list(new_circuit[i].values()).index(ii)
+                                key = list(new_circuit[i].keys())[index]   # index you want
+                                new_circuit[i].pop(key)
+                                
+                            else:
+                                new_circuit[i] = new_circuit[i]|loc_circuit[i]
+            else:
+                new_circuit = deepcopy(self.failing_circuits[-1][j])
+            new_circuit_all.append(new_circuit)
+
+        new_subset = self.calculate_subset(new_circuit_all)
+        pi_E_new = self.subset_probs[new_subset]
+        q = pi_E_new/self.pi_E
+        if np.random.rand(1)<q:
+            fail_round =  self.run(new_circuit_all, n_rounds)
+            if fail_round is not None:
+                self.failing_times.append(fail_round)
+                self.failing_circuits.append(new_circuit_all)
+                self.pi_E = pi_E_new
+                self.subsets.append(new_subset)
+        
+    def run(self, fault_circuits, n_rounds):
+
+        round = 0
+        fail_round = None
+        pnode = self.protocol.root # get protocol start node
+        state = self.simulator(max(self.protocol.qubits)+1) # init state
+        msmt_hist = {} # init measurement history
+        while True:
+            pnode, circuit = self.protocol.successor(pnode, msmt_hist)
+            if circuit != None:
+                if not circuit.noisy:
+                    msmt = state.run(circuit)
+                else:
+                    msmt = state.run(circuit, fault_circuits[round])
+
+                msmt = msmt if msmt==None else int(msmt,2) # convert to int for comparison in checks
+                msmt_hist[pnode] = msmt_hist.get(pnode, []) + [msmt]
+            else:
+                round += 1
+                if pnode != None:
+                    fail_round = np.copy(round)
+                if round < n_rounds:
+                    pnode = self.protocol.root
+                    msmt_hist = {}
+                else:
+                    return fail_round
+            
